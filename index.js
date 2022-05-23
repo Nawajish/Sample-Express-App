775caf230d0a91334210f12df30cb734c0c53508
Nawaz <laskarnawajish@gmail.com>
2022-05-23 12:54:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
 const createZip = require('./create-zip');
 
+app.get('/', (req, res) => {
+  res.send('Server is up!');
+})
+
 app.get('/raml', (req, res) => {
-  res.set('Content-Type', 'text/plain');
+  const ramlSpec = fs.readFileSync('./raml-spec.yml', 'utf8');
+  const ramlSpecString = ramlSpec.split('\r\n').join('\n');
+
   res.send(ramlSpecString);
 });
 
