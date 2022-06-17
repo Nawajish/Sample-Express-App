bf2a3d2c0e6cae0e9fca026c3a34d9e8eb14f78f
Nawaz <laskarnawajish@gmail.com>
2022-06-17 20:39:27 +0530

    Added additional routes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const createZip = require('./create-zip');
 
 app.get('/', (req, res) => {
   res.send('Server is up!');
-})
+});
 
 app.get('/raml', (req, res) => {
   const ramlSpec = fs.readFileSync('./raml-spec.yml', 'utf8');
@@ -28,6 +28,20 @@ app.get('/user/:userId', (req, res) => {
   res.send(`User ${userId}`);
 });
 
+// create PUT route for /user/:userId
+app.put('/user/:userId', (req, res) => {
+  // get userId from request params or query string or body
+  const userId = req.params?.userId || req.body?.userId;
+  res.send(`User ${userId}`);
+});
+
+// create DELETE route for /user/:userId
+app.delete('/user/:userId', (req, res) => {
+  // get userId from request params or query string or body
+  const userId = req.params?.userId || req.body?.userId;
+  res.send(`User ${userId}`);
+});
+
 app.listen(process.env.PORT || 3131, () => {
-    console.log('Server listening on port 3131');
+  console.log('Server listening on port 3131');
 });
